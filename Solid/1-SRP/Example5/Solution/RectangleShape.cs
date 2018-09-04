e3ec5d80ae8cee044c20128c94901c895f7f5a8e
Fabio Tavoloni <fabio.t0844.beyond@xpi.com.br>
2018-09-04 17:03:49 -0300

    SRP

---
+++ Solid/1-SRP/Example5/Solution/RectangleShape.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solid._1_SRP.Example5.Solution
+{
+    //class that calculate area
+    public class RectangleShape
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public int Area() => Width * Height;
+    }
+
+    //class that draw something
+    public class RectangleDraw
+    {
+        public void Draw(string form) => Console.WriteLine("Drawing: " + form);
+    }
+
+    //consume rectangle shape
+    public class GeometricsCalculator
+    {
+        public int CalculateArea(RectangleShape recShape) => recShape.Area();
+    }
+
+    //consumes rectangle draw and shape
+    public class GraphicsManager
+    {
+        public string Form { get; set; }
+
+        public void Draw(RectangleDraw recDraw, RectangleShape recShape) {
+            recDraw.Draw("asdasdasd");
+            Console.WriteLine("Drawing: ");
+        }
+    }
+}
---
+++ Solid/1-SRP/Example5/Violation/Rectangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solid._1_SRP.Example5
+{
+    //class to calculate area and draw something
+    public class RectangleShape
+    {
+        public  int Height { get; set; }
+        public  int Width { get; set; }
+
+        public  int Area() => Width * Height;
+
+        public  void Draw(string form) => Console.WriteLine("Drawing: " + form);
+    }
+
+    //class that consumes the Rectangle
+    public class GeometricsCalculator
+    {
+        public int CalculateArea(RectangleShape recShape) => recShape.Area();
+    }
+
+    //another actor using the Rectangle class
+    public class GraphicsManager
+    {
+        public void DrawOnScreen(RectangleShape recShape) => recShape.Draw("Adasdasdasdasd");
+    }
+}

31c709f537c2b7dbcea3e678aefa54ac2156665a
Fabio Tavoloni <fabio.t0844.beyond@xpi.com.br>
2018-09-04 17:17:56 -0300

    SRP with interfaces

---
+++ Solid/1-SRP/Example6/Solution/IEmail.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solid._1_SRP.Example6.Solution
+{
+    interface IEmail
+    {
+        void setSender(string sender);
+        void setReceiver(string receiver);
+        void setContent(IContent content);
+    }
+
+    //change protocol changes only the Email class
+    public interface IContent
+    {
+        string getAsString(); //use for serialization
+    }
+
+    //change content causes changes in Content class
+    public class Email : IEmail
+    {
+        public void setContent(IContent content)
+        {
+            Console.WriteLine("set content");
+        }
+
+        public void setReceiver(string receiver)
+        {
+            Console.WriteLine("set receiver");
+        }
+
+        public void setSender(string sender)
+        {
+            Console.WriteLine("set sender");
+        }
+    }
+}
---
+++ Solid/1-SRP/Example6/Violation/IEmail.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solid._1_SRP.Example6.Violation
+{
+    interface IEmail
+    {
+        void setSender(string sender);
+        void setReceiver(string receiver);
+        void setContent(string content);
+    }
+
+    //this class has two reasons to change: one is change of protocol for send and receive
+    //another is the change of content type (HTML)
+    public class Email : IEmail
+    {
+        public void setContent(string content)
+        {
+            Console.WriteLine("set content");
+        }
+
+        public void setReceiver(string receiver)
+        {
+            Console.WriteLine("set receiver");
+        }
+
+        public void setSender(string sender)
+        {
+            Console.WriteLine("set sender");
+        }
+    }
+}

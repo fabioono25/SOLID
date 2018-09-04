c12bb733a8f421f3395c66329feae942e9251634
Fabio Tavoloni <fabio.t0844.beyond@xpi.com.br>
2018-09-04 11:33:33 -0300

    Repository with SRP

---
+++ Solid/1-SRP/Example4/Solution/IRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Text;
+
+namespace Solid._1_SRP.Example4.Solution
+{
+    public interface IRepository<T> where T : class
+    {
+        void Create(T entity);
+        void Save();
+    }
+
+    public class Repository<D, T> : IRepository<T> where T : class where D : DbContext, new()
+    {
+        readonly D entities = new D();
+
+        public void Create(T entity)
+        {
+            try
+            {
+                entities.Set<T>().Add(entity);
+            }
+            catch (Exception ex)
+            {
+                FileLogger.Log(ex.ToString());
+            }
+
+        }
+
+        public void Save()
+        {
+            try
+            {
+                entities.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                FileLogger.Log(ex.ToString());
+            }
+        }
+    }
+
+    internal static class FileLogger
+    {
+        private static string Path { get; } = @"C:\Temp";
+
+        public static void Log(string message) => File.WriteAllText(Path, message);
+    }
+}
---
+++ Solid/1-SRP/Example4/Violation/IRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Text;
+
+namespace Solid._1_SRP.Example4.Violation
+{
+    public interface IRepository<T> where T : class
+    {
+        void Create(T entity);
+        void Save();
+    }
+
+    public class Repository<D, T> : IRepository<T> where T : class where D : DbContext, new()
+    {
+        readonly D entities = new D();
+
+        //two different responsibilities: stores data and logs the error
+        //repository is for data access logic
+        public void Create(T entity)
+        {
+            try
+            {
+                entities.Set<T>().Add(entity);
+            }
+            catch (Exception ex)
+            {
+                File.WriteAllText(@"error.log", ex.ToString());
+            }
+            
+        }
+
+        public void Save()
+        {
+            try
+            {
+                entities.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                File.WriteAllText(@"error.log", ex.ToString());
+            }
+        }
+    }
+}

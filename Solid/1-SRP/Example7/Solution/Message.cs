6e2552a9cfefa1ba71345a682fc61da1c609ffbe
Fabio Tavoloni <fabio.t0844.beyond@xpi.com.br>
2018-09-05 09:19:28 -0300

    SRP Example 7

---
+++ Solid/1-SRP/Example7/Solution/Message.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solid._1_SRP.Example7.Solution
+{
+    public interface IMessageServer
+    {
+        bool Send(IMessage message);
+    }
+
+    public interface IMessage
+    {
+        IList<string> ToAddresses { get; set; }
+        string MessageBody { get; set; }
+    }
+
+    public class SmtpMessage : IMessage
+    {
+        public IList<string> ToAddresses { get; set; }
+        public string MessageBody { get; set; }
+
+        public bool Send()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class SmsMessage : IMessage
+    {
+        public IList<String> ToAddresses { get; set; }
+        public string MessageBody { get; set; }
+    }
+    public class SmsMessageServer : IMessageServer
+    {
+        public bool Send(IMessage message)
+        {
+            //Do the real work here
+            return true;
+        }
+    }
+
+    public class SmtpMessageServer : IMessageServer
+    {
+        public bool Send(IMessage message)
+        {
+            //Do the real work here
+            return true;
+        }
+    }
+}
---
+++ Solid/1-SRP/Example7/Violation/Message.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solid._1_SRP.Example7.Violation
+{
+    public interface IMessage
+    {
+        IList<string> ToAddresses { get; set; }
+        string MessageBody { get; set; }
+        bool Send();
+    }
+
+    public interface IMailMessage: IMessage
+    {
+        string Subject { get; set; }
+        IList<string> BccAddresses { get; set; }
+    }
+
+    public class SmtpMessage : IMailMessage
+    {
+        public IList<string> ToAddresses { get; set; }
+        public IList<string> BccAddresses { get; set; }
+        public string Subject { get; set; }
+        public string MessageBody { get; set; }
+
+        public bool Send()
+        {
+            //do real work here
+            throw new NotImplementedException();
+        }
+    }
+
+    public class SmsMessage : IMessage
+    {
+        public IList<string> ToAddresses { get; set; }
+        public string MessageBody { get; set; }
+
+        public bool Send()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

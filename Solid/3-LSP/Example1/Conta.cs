917cdf01829145ec27371f0802a2d2abc053bd77
Fabio Tavoloni <fabio.t0844.beyond@xpi.com.br>
2018-09-21 10:01:34 -0300

    Liskov

---
+++ Solid/3-LSP/Example1/Conta.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solid._3_LSP.Example1
+{
+    class Conta
+    {
+        public virtual int Id { get; set; }
+        public Conta(int contaId)
+        {
+            Id = contaId;
+        }
+
+        public virtual void Saque(decimal valor) => Console.WriteLine("saque na classe base");
+    }
+
+    class ContaPoupanca : Conta
+    {
+        public ContaPoupanca(int contaPoupancaId) : base(contaPoupancaId)
+        {
+        }
+
+        public override void Saque(decimal valor) => Console.WriteLine("saque na classe derivada");
+    }
+
+    public static class Sample
+    {
+        public static void ExecuteSample()
+        {
+            var c1 = new Conta(1);
+            c1.Saque(12);
+
+            //usando Liskov
+            var c2 = new ContaPoupanca(2);
+            c2.Saque(20);
+        }
+    }
+}
